2d3cd004baa8464382f863b96795e66eaac72306
scpsnowflake <115984870+scpsnowflake@users.noreply.github.com>
2022-10-17 05:39:32 -0400

    Update streamlit_app.py
    Changed header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 streamlit.title("My Parents New Healty Diner")
 
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket smoothie")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Hard boiled free ranged egg")
+streamlit.text("Avocado Toast")
