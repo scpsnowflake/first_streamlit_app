a31dde51ce1fc5389ba040b3dcf2f6cca5f9bdce
scpsnowflake <115984870+scpsnowflake@users.noreply.github.com>
2022-10-17 05:42:33 -0400

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,9 @@ import streamlit
 streamlit.title("My Parents New Healty Diner")
 
 streamlit.header("Breakfast Favorites")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket smoothie")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Hard boiled free ranged egg")
-streamlit.text("Avocado Toast")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket smoothie")
+streamlit.text("🐔 Hard boiled free ranged egg")
+streamlit.text("🥑🍞 Avocado Toast")
 
-streamlit.header("Build Your Own Fruit Smoothie")
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
